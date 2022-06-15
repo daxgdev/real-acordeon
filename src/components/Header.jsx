f93751c824bbf40bc47515f7933b37292f1d88cd
jersonsilvaa <jerson.silva99@hotmail.com>
2022-06-15 17:07:35 -0500

    #10 [Añadiendo cambio de color al header al hacer scroll]

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,23 @@ import { NavLink } from "react-router-dom";
 import "../styles/components/Header.scss";
 
 export const Header = () => {
+  const [color, setColor] = useState(false) // Crear hook de useState
   const [isActive, setIsActive] = useState(false);
 
+  const changeColor = () => {
+    // El scrollY controla cuando se activará el color
+    if (window.scrollY >= 90) {
+      setColor(true);
+    } else {
+      setColor(false);
+    }
+  };
+
+  // Escuchar al evento del scroll, cuando lo haga llama a la función changeColor
+  window.addEventListener("scroll", changeColor);
+
   return (
-    <header className="header">
+    <header className={color ? 'header header__bg' : 'header'}>
       <section className="header_container">
         <NavLink to="/" className="header_logo">
           logo
